763a709d2080ff97ac285ca21447671bb7ee5aa7
jo0707 <josua123690707@gmail.com>
2025-02-10 00:13:09 +0700

    feat: new projects

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,33 +1,34 @@
 export default defineNuxtConfig({
-    modules: ["@hypernym/nuxt-anime", "@vueuse/nuxt", "@nuxt/ui", "@nuxt/fonts", "@nuxtjs/seo", "nuxt-gtag"],
+  modules: ["@hypernym/nuxt-anime", "@vueuse/nuxt", "@nuxt/ui", "@nuxt/fonts", "@nuxtjs/seo", "nuxt-gtag"],
 
-    anime: {
-        composables: true,
-        autoImport: true,
-    },
+  anime: {
+      composables: true,
+      autoImport: true,
+  },
 
-    colorMode: {
-        preference: "light",
-    },
+  colorMode: {
+      preference: "light",
+  },
 
-    ui: {
-        global: true,
-        safelistColors: ["amber", "green", "sky", "blue", "rose"],
-    },
+  ui: {
+      global: true,
+      safelistColors: ["amber", "green", "sky", "blue", "rose"],
+  },
 
-    site: {
-        url: "https://jooo.site",
-        name: "Joshua Sinaga's Portofolio",
-        description:
-            "Joshua Palti Sinaga is a web and mobile developer and currently an informatics engineering student. Love to learn new things and solving problems around me.",
-        defaultLocale: "en",
-    },
+  site: {
+      url: "https://jooo.site",
+      name: "Joshua Sinaga's Portofolio",
+      description:
+          "Joshua Palti Sinaga is a web and mobile developer and currently an informatics engineering student. Love to learn new things and solving problems around me.",
+      defaultLocale: "en",
+  },
 
-    gtag: {
-        enabled: process.env.NODE_ENV === "production",
-        id: "G-VJPTRRC2EF",
-    },
+  gtag: {
+      enabled: process.env.NODE_ENV === "production",
+      id: "G-VJPTRRC2EF",
+  },
 
-    devtools: { enabled: false },
-    devServer: { host: "127.0.0.1", port: 3000 },
+  devtools: { enabled: false },
+  devServer: { host: "127.0.0.1", port: 3000 },
+  compatibilityDate: "2025-02-09",
 })
